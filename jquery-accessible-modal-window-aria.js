ef0db3464e25362c3334b7b6b4210eca4ad85460
Nicolas Hoffmann <dante3333@gmail.com>
2016-02-12 15:46:48 +0100

    Add animation + fix bug modal opened
    - Bug if modal is already opened and focus in the browser bar => focus is trapped and goes into the modal, if opened.
    
    - add animation possibilities
    
    Closed #7 and #10

--- jquery-accessible-modal-window-aria.js
+++ jquery-accessible-modal-window-aria.js
@@ -11,26 +11,25 @@ $(document).ready(function(){
    var $modals = $( '.js-modal' ),
        $body = $( 'body' );
 
-   if ( $modals.length  ) { // if there are at least one :)
-      $modals.each( function(index_to_expand) {
-          var $this = $(this) ,
-              index_lisible = index_to_expand+1;
+   $modals.each( function(index_to_expand) {
+       var $this = $(this) ,
+           index_lisible = index_to_expand+1;
           
-          $this.attr({
-                  'id' : 'label_modal_' + index_lisible
-                });
+       $this.attr({
+             'id' : 'label_modal_' + index_lisible
+              });
 
-      });
+   });
       
-      // jQuery formatted selector to search for focusable items
-      var focusableElementsString = "a[href], area[href], input:not([disabled]), select:not([disabled]), textarea:not([disabled]), button:not([disabled]), iframe, object, embed, *[tabindex], *[contenteditable]";
+   // jQuery formatted selector to search for focusable items
+   var focusableElementsString = "a[href], area[href], input:not([disabled]), select:not([disabled]), textarea:not([disabled]), button:not([disabled]), iframe, object, embed, *[tabindex], *[contenteditable]";
       
-      if ( $('#js-modal-page').length === 0 ) { // just to avoid missing #js-modal-page
-         $body.wrapInner('<div id="js-modal-page"></div>');
-         }
+   if ( $('#js-modal-page').length === 0 ) { // just to avoid missing #js-modal-page
+       $body.wrapInner('<div id="js-modal-page"></div>');
+       }
       
-      // events ------------------
-      $body.on( 'click', '.js-modal', function( event ) {
+   // events ------------------
+   $body.on( 'click', '.js-modal', function( event ) {
          var $this = $(this),
              options = $this.data(),
              $modal_starter_id = $this.attr('id'),
@@ -79,36 +78,59 @@ $(document).ready(function(){
          
          event.preventDefault();
          
-      })
-      // close button and esc key
-      $body.on( 'click', '#js-modal-close', function( event ) {
+   })
+   // close button and esc key
+   $body.on( 'click', '#js-modal-close', function( event ) {
          var $this = $(this),
              $focus_back = '#' + $this.attr('data-focus-back'),
              $js_modal = $('#js-modal'),
+             $class_element = $js_modal.attr('class'),
+             $class_element_reverse = $class_element + '--reverse',
              $js_modal_overlay = $('#js-modal-overlay'),
              $page = $('#js-modal-page');
              
          $page.removeAttr('aria-hidden');
-         $body.removeClass('no-scroll');
-         $js_modal.remove();
-         $js_modal_overlay.remove();
-         $( $focus_back ).focus();
-         
-      })
-      .on( 'click', '#js-modal-overlay', function( event ) {
-         var $close = $('#js-modal-close');
 
+         var delay = $js_modal.css( "animation-duration" );
+         if ( delay != '0s' ){
+             var timeout = parseFloat(delay.replace('s','')) * 1000;
+             timeout++;
+			 
+             $js_modal.removeClass( $class_element );
+             setTimeout(function() { $js_modal.addClass( $class_element_reverse ); }, 1);
+             setTimeout(function() {
+                  $body.removeClass('no-scroll');
+                  $js_modal.remove();
+                  $js_modal_overlay.remove();
+                  $( $focus_back ).focus();	
+                  $js_modal.removeClass( $class_element_reverse );
+                  $js_modal.addClass( $class_element );
+                }, timeout);
+            }
+            else {
+                  $body.removeClass('no-scroll');
+                  $js_modal.remove();
+                  $js_modal_overlay.remove();
+                  $( $focus_back ).focus();			
+                 }
+     
+   })
+   .on( 'click', '#js-modal-overlay', function( event ) {
+         var $close = $('#js-modal-close');
+         
+         event.preventDefault();
          $close.trigger('click');
 		 
-      })
-      .on( 'keydown', '#js-modal-overlay', function( event ) {
+   })
+   .on( 'keydown', '#js-modal-overlay', function( event ) {
           var $close = $('#js-modal-close');
 
-	      if ( event.keyCode == 13 || event.keyCode == 32 ) { // space or enter
-             $close.trigger('click');
-             }
-      })
-      .on( "keydown", "#js-modal", function( event ) {
+          if ( event.keyCode == 13 || event.keyCode == 32 ) { // space or enter
+		          event.preventDefault();
+              $close.trigger('click');
+              }
+   })
+   .on( "keydown", "#js-modal", function( event ) {
          var $this = $(this),
 		     $close = $('#js-modal-close');
          
@@ -143,13 +165,22 @@ $(document).ready(function(){
            
             }
          
-      })
-      .on( 'focus', '#js-modal-tabindex', function( event ) {
+   })
+   .on( "keyup", ":not(#js-modal)", function( event ) {
+          var $this = $(this),
+              $js_modal = $('#js-modal'),
+              focusedItem = $( document.activeElement ),
+              in_jsmodal = focusedItem.parents('#js-modal').length ? true : false;
+              $close = $('#js-modal-close');
+          
+          if ( $js_modal.length && event.keyCode == 9 && in_jsmodal === false ) { // tab or maj+tab
+             $close.focus();
+          }
+
+   })
+   .on( 'focus', '#js-modal-tabindex', function( event ) {
          $close.focus(); 
-      });
-   
-   
-   }
+   });
  
   
 });
