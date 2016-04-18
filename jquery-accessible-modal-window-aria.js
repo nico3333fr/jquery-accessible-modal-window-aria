fae8fbe817e4744c05ccd3f824086d16b34ff610
Nicolas Hoffmann <dante3333@gmail.com>
2016-04-18 16:32:32 +0200

    Added class for close text
    it puts a span class="<prefix>-modal-close__text" in the clode button, easier to style.

--- jquery-accessible-modal-window-aria.js
+++ jquery-accessible-modal-window-aria.js
@@ -47,7 +47,7 @@ $(document).ready(function(){
          
          // insert code at the end
          $modal_code = '<dialog id="js-modal" class="' + $modal_prefix_classes + 'modal" role="dialog" aria-labelledby="modal-title"><div role="document">';
-         $modal_code += '<button id="js-modal-close" class="' + $modal_prefix_classes + 'modal-close" data-content-back-id="' + modal_content_id + '" data-focus-back="' + $modal_starter_id + '" title="' + $modal_close_title + '">' + $modal_close_text + '</button>';
+         $modal_code += '<button id="js-modal-close" class="' + $modal_prefix_classes + 'modal-close" data-content-back-id="' + modal_content_id + '" data-focus-back="' + $modal_starter_id + '" title="' + $modal_close_title + '"><span class="' + $modal_prefix_classes + 'modal-close__text">' + $modal_close_text + '</span></button>';
          if ($modal_title !== ''){
             $modal_code += '<h1 id="modal-title" class="' + $modal_prefix_classes + 'modal-title">' + $modal_title + '</h1>';
             }
